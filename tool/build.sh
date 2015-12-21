d73a2bcbbe416f9e9f19bc21f5d6134bb97cae78
Kenneth Endfinger <kaendfinger@gmail.com>
2015-12-20 22:07:14 -0500

    Copy pubspec.yaml

--- tool/build.sh
+++ tool/build.sh
@@ -9,6 +9,6 @@ touch stub
 pub get
 cp -R -L packages build/packages
 cp -R bin lib build/
-cp dslink.json build/
+cp pubspec.yaml dslink.json build/
 cd build
 zip -r ../../../files/dslink-dart-rpi.zip .
